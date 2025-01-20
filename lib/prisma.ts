a4a4e5a4c6cf99e30cef2b1f03e379362bac584f
Arjun Sharma <arjunsharma03.sa@gmail.com>
2025-01-20 07:19:35 +0530

    Var removed to handle vercel linting

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-    // hold instance of PrismaClient
-    var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-// @ts-ignore: Props data will be handled dynamically
-let prisma: any;
-if(process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-}else{
-    if(!global.prisma){
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
-export default prisma;
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
